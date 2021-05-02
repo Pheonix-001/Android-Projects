47ff26014fcf4746cce46eb1a16c16ed94537509
Pheonix-001 <diplomadear001@gmail.com>
2021-05-02 19:39:26 +0530

    lksdj

--- app/src/main/java/com/example/fuckedupapp/MainActivity.java
+++ app/src/main/java/com/example/fuckedupapp/MainActivity.java
@@ -35,6 +35,8 @@ public class MainActivity extends AppCompatActivity {
 
                 System.out.println("sdfj");
 
+                System.out.println("lsdfjlkg");
+
             }
         });
 
