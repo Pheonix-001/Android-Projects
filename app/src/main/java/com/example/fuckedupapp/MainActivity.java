cae7131214e9400bd7a8cc65b7062c38351b5d3c
Pheonix-001 <diplomadear001@gmail.com>
2021-05-04 16:30:50 +0530

    Menu Fragments added

--- app/src/main/java/com/example/fuckedupapp/MainActivity.java
+++ app/src/main/java/com/example/fuckedupapp/MainActivity.java
@@ -1,15 +1,17 @@
 package com.example.fuckedupapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottom_nav;
 
 
     @Override
@@ -17,8 +19,38 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        bottom_nav = findViewById(R.id.bottom_nav);
 
+        bottom_nav.setOnNavigationItemSelectedListener(bottomItemSelectListener);
 
-
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener bottomItemSelectListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                    Fragment selectedFragment = null;
+
+                    switch(item.getItemId()) {
+                        case R.id.home_icon :
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.call_icon :
+                            selectedFragment = new CallFragment();
+                            break;
+                        case R.id.message_icon :
+                            selectedFragment = new MessageFragment();
+                            break;
+                        case R.id.profile_icon :
+                            selectedFragment = new ProfileFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+
+                    return true;
+                }
+            };
 }
